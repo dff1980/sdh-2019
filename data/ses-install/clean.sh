f5b94a19c18b33609e4fc6869b7afd2842e75ae5
Pavel Zhukov <Pavel.Zhukov@suse.com>
2019-03-25 13:42:39 +0300

    new file:   data/ses-install/clean.sh 	new file:   data/ses-install/restart.sh

---
+++ data/ses-install/clean.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+salt-run  state.orch ceph.stage.5
+salt '*' grains.delval deepsea destructive=True
+salt-key -D -y
+zypper rm -y deepsea salt-api salt-minion salt-master salt ceph-common
+rm -r /srv/modules
+rm -r /srv/pillar
+rm -r /srv/salt
+rm -r /etc/salt
+rm -r /var/cache/salt
+rm /srv/pillar/ceph/master_minion.sls.rpmsave /srv/pillar/ceph/deepsea_minions.sls.rpmsave /etc/salt/master.d/sharedsecret.conf.rpmsave
+for i in 1 2 3 4; do ssh osd-0$i 'zypper rm -y salt salt-minion ceph-common; rm -r /etc/salt /var/lib/ceph /var/cache/salt /etc/salt; echo -e "d\n2\nd\nw\n" | fdisk /dev/sdb; echo -e "d\n2\nd\nw\n" | fdisk /dev/sdc; echo -e "d\n2\nd\nw\n" | fdisk /dev/sdd; reboot'; done
+
+sleep 30
+
+for i in 1 2 3 4
+ do
+  echo "wait to start osd-0$i after reboot"
+  until (ping osd-0$i -c 1 > /dev/null) ; do sleep 10; done
+ done
+
+sleep 30
+
+for i in 1 2 3 4; do ssh osd-0$i 'rm -r /var/lib/ceph'; done
+
+
